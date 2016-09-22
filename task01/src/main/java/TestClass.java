14675964c3408c4a19abeabcf332df5fc10b402c
Artem Trofimov <atrofimov@lohika.com>
2016-09-22 14:41:53 +0300

    added cycle - commit2

---
+++ task01/src/main/java/TestClass.java
@@ -0,0 +1,10 @@
+public class TestClass {
+
+    public static void main(String[] args) {
+        System.out.println("HH W!");
+
+        for (int i = 0; i <; i++) {
+            System.out.println(i);
+        }
+    }
+}
